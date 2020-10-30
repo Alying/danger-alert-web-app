d05f4c8cef38d727ac108b3b5b94c4793fa7768e
Alina Ying <alina.ying@gmail.com>
2020-10-29 23:47:09 -0700

    added location request popup in main.js

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/**
+ * Description: Handles location request popup window for the Danger Alert application.
+ * Author: Alina Ying
+ * Author URI: https://alinaying.com/
+ * Tags: Website
+ */
+
+function openLocationRequest() {
+    window.alert('Allow location to be accessed?');
+    window.location.href='components/search_page.html';
+}
