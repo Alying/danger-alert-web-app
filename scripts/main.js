5031c909b6933249437f3ee71fc0444db11e414c
Alina Ying <alina.ying@gmail.com>
2020-10-30 01:41:23 -0700

    fixed openLocationRequests so that Danger Button will go in a loop

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,19 @@
  */
 
 function openLocationRequest() {
-    window.alert('Allow location to be accessed?');
+    window.alert('Allow location to be accessed?)'.bold() +  '\nYour current location is 66 Ceres St\n' +
+        'San Francisco, California(CA), 94124');
     window.location.href='components/search_page.html';
+}
+
+function openLocationRequest2() {
+    window.alert('Allow location to be accessed?'.bold() + '\nYour current location is 8253 W. Glenholme Drive\n' +
+        'El Paso, TX 79915');
+    window.location.href='search_page2.html';
+}
+
+function openLocationRequest3() {
+    window.alert('Allow location to be accessed?)'.bold() +  '\nYour current location is 66 Ceres St\n' +
+        'San Francisco, California(CA), 94124');
+    window.location.href='search_page.html';
 }
